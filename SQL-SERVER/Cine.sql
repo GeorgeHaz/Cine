d82bc458d9be8b36cdbd56fa43b244cc9d5d0b4a
George Haz <104809599+GeorgeHaz@users.noreply.github.com>
2025-05-01 15:52:44 -0500

    Update Cine.sql
    Se reestructuro las tablas

--- SQL-SERVER/Cine.sql
+++ SQL-SERVER/Cine.sql
@@ -1,35 +1,59 @@
-CREATE DATABASE Cine
+GO
+CREATE DATABASE CINE
+GO
+USE [Cine]
 GO
 
-USE Cine
+CREATE TABLE [dbo].[pelicula](
+	[id_pelicula] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](max) NOT NULL,
+	[duracion] [int] NOT NULL,
+	[fecha_publicacion] [datetime2](7) NOT NULL,
+	[audit_delete_date] [datetime2](7) NULL,
+	[audit_delete_user] [int] NULL,
+ CONSTRAINT [PK_pelicula] PRIMARY KEY CLUSTERED 
+(
+	[id_pelicula] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+CREATE TABLE [dbo].[sala_cine](
+	[id_sala] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](max) NOT NULL,
+	[estado] [nvarchar](max) NOT NULL,
+	[audit_delete_date] [datetime2](7) NULL,
+	[audit_delete_user] [int] NULL,
+ CONSTRAINT [PK_sala_cine] PRIMARY KEY CLUSTERED 
+(
+	[id_sala] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+CREATE TABLE [dbo].[pelicula_salacine](
+	[id_pelicula_sala] [int] IDENTITY(1,1) NOT NULL,
+	[salaCineId] [int] NOT NULL,
+	[peliculaId] [int] NOT NULL,
+	[fecha_publicacion] [datetime2](7) NOT NULL,
+	[fecha_fin] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_pelicula_salacine] PRIMARY KEY CLUSTERED 
+(
+	[id_pelicula_sala] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-CREATE TABLE pelicula(
-	id_pelicula INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	nombre NVARCHAR(255) NOT NULL,
-	duracion INT NOT NULL,
-	activo INT NOT NULL,
-	fecha_eliminacion DATETIME
-)
+ALTER TABLE [dbo].[pelicula_salacine]  WITH CHECK ADD  CONSTRAINT [FK_pelicula_salacine_pelicula_peliculaId] FOREIGN KEY([peliculaId])
+REFERENCES [dbo].[pelicula] ([id_pelicula])
+ON DELETE CASCADE
 GO
 
-CREATE TABLE sala_cine(
-	id_sala_cine INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	nombre NVARCHAR(255) NOT NULL,
-	estado INT NOT NULL,
-	activo INT NOT NULL,
-	fecha_eliminacion DATETIME
-)
+ALTER TABLE [dbo].[pelicula_salacine] CHECK CONSTRAINT [FK_pelicula_salacine_pelicula_peliculaId]
 GO
 
-CREATE TABLE pelicula_salacine(
-	id_pelicula_sala INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	id_sala_cine INT NOT NULL,
-	fecha_publicacion DATE NOT NULL,
-	fecha_fin DATE NOT NULL,
-	id_pelicula INT NOT NULL,
+ALTER TABLE [dbo].[pelicula_salacine]  WITH CHECK ADD  CONSTRAINT [FK_pelicula_salacine_sala_cine_salaCineId] FOREIGN KEY([salaCineId])
+REFERENCES [dbo].[sala_cine] ([id_sala])
+ON DELETE CASCADE
+GO
 
-	CONSTRAINT fk_pelicula_salacine_sala_cine FOREIGN KEY(id_sala_cine) REFERENCES sala_cine(id_sala_cine),
-	CONSTRAINT fk_pelicula_salacine_pelicula FOREIGN KEY(id_pelicula) REFERENCES pelicula(id_pelicula)
-)
+ALTER TABLE [dbo].[pelicula_salacine] CHECK CONSTRAINT [FK_pelicula_salacine_sala_cine_salaCineId]
 GO
